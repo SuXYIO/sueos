09e7b90583836ed036acdd3f2156c77014abc256
SueXY <SuXYMC@163.com>
2025-06-30 22:34:48 +0800

    Update runemu.sh

--- runemu.sh
+++ runemu.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # run bochs emulation for the image
 
-if [ "$#" -eq 0 ]; then
-	echo 'No args spec, using default.'
-	echo '(Executing bochs -f ./bochsrc.bxrc.)'
-	bochs -f bochsrc.bxrc
-elif [ "$#" -gt 0 ]; then
-	bochs -f bochsrc.bxrc "$*"
-fi
+bochs -f bochsrc.bxrc "$*"
